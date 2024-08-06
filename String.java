4fbfa32bf46598d5c24848ac05112992d4b4beb3
rajputrukmani <169047459+rajputrukmani@users.noreply.github.com>
2024-08-06 15:35:29 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,15 @@
+public class String10 {
+    public static void main(String[] args) {
+        ///*lowercase */
+        String strA[];
+        String str = "The Quick BroWn FoX!";
+        System.out.println(str.toLowerCase());
+
+        //uppercase
+        String str1 = "the quick brown fox!";
+        System.out.println(str1.toUpperCase());
+      
+        
+    }
+    
+}
