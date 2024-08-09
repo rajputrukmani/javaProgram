90b79ff87f8a46776826737f2875aa38d13d0a34
rajputrukmani <169047459+rajputrukmani@users.noreply.github.com>
2024-08-09 16:00:50 +0530

    Create Pattern2.java

---
+++ Pattern2.java
@@ -0,0 +1,18 @@
+public class Pattern {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=5;i++)
+        {
+      
+            for(j=1;j<=i*2;j++)
+            {
+        
+                System.out.print(j);
+            }
+          i++;
+            System.out.println();
+
+        }
+    }
+    
+}
