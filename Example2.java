a34ac2ff7a02d966a44dd3ae1bc7c9c977448a93
rajputrukmani <169047459+rajputrukmani@users.noreply.github.com>
2024-08-15 23:35:44 +0530

    Create Example2.java

---
+++ Example2.java
@@ -0,0 +1,14 @@
+//write a ja p to take to float value and aply relation operater
+public class Example1 {
+    public static void main(String[] args) {
+       float a=10.5f;
+        float b=20f;
+        System.out.println("less than="+(a<b)); 
+        System.out.println("greater than="+(a>b));
+        System.out.println("less than qual="+(a<=b));
+        System.out.println("greater than qual="+(a>=b));
+        System.out.println("qual to="+(a==b));
+        System.out.println("not qual="+(a!=b));
+
+    }
+  }
