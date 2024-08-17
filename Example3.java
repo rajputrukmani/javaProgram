b2dcb91e3d8bf1f25f3863282af6dee12e3cf11b
rajputrukmani <169047459+rajputrukmani@users.noreply.github.com>
2024-08-17 10:27:06 +0530

    Create Example3.java

---
+++ Example3.java
@@ -0,0 +1,10 @@
+//1.float value are assigned using f
+class HelloWorld{
+public static void main(String[]args) 
+{
+float f=23;
+float l=5.67f;
+System.out.println("f="+f);
+System.out.println("l="+l);
+}
+}
