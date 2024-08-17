4d86ea77a23662cfe1971833de646192ba6066f5
rajputrukmani <169047459+rajputrukmani@users.noreply.github.com>
2024-08-17 10:30:29 +0530

    Create Demo8.java

---
+++ Demo8.java
@@ -0,0 +1,27 @@
+public class Student789{
+    static String college="Infobeans";
+    String name;
+
+    Student789(String name)
+    {
+        this.name=name;
+    }
+
+static String details ()
+{
+    System.out.println(college);
+
+return "data1";
+}
+
+static  void data()
+{
+    System.out.println("Bhavna");
+}
+public static void main(String[] args) {
+    Student789 ob=new Student789("ram");
+    details();
+    data();
+}
+
+}
