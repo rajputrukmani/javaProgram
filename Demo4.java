297b5185fe2794eeab2ef7061a65fad782c08689
rajputrukmani <169047459+rajputrukmani@users.noreply.github.com>
2024-08-21 12:21:04 +0530

    Create Demo4.java

---
+++ Demo4.java
@@ -0,0 +1,16 @@
+  class Demo5 {
+    public static void main(String[] args) {
+        int i, j;
+        int c=1;
+        for(i=1;i<=5;i++)
+         {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(c);
+        c++;
+        }
+            System.out.println();
+        }
+    }
+    
+}
