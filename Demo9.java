8b6056b754dbaabc9516666f95caec888f21cdd5
rajputrukmani <169047459+rajputrukmani@users.noreply.github.com>
2024-09-04 08:05:52 +0530

    ParameterConstructor

---
+++ Demo9.java
@@ -0,0 +1,33 @@
+ class Person
+  {
+    int id;
+    String name;
+    Person(int id,String name)
+    {
+      this.id=id;
+      this.name=name;
+    }
+  }
+class Employee extends Person
+  {
+    float salary;
+    Employee(int id,String name,float salary)
+    {
+      super(id,name);
+      this.salary=salary;
+    }
+
+    void display()
+    {
+      System.out.println(id + " "+ name+" "+ salary);
+    }
+  }
+    public class Driver
+      {
+        public static void main(String[]args)
+        {
+          Employee e1 = new Employee(1,"Rukmani",30000);
+             e1.display();
+        }
+      }
+  
