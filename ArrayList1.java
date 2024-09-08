14861d3624cef1599c3fafb1c275abf655052315
rajputrukmani <169047459+rajputrukmani@users.noreply.github.com>
2024-09-08 21:32:08 +0530

    Create ArrayList1.java

---
+++ ArrayList1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Demo1
+{
+    public static void main(String[] args) {
+       //Q.1
+         ArrayList <String> st = new ArrayList<>();
+         st.add("blue");
+         st.add("red");
+        st.add("black");
+       System.out.println(st);
+    }
+}
